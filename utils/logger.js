0d620e210705697c2da88f291e2bb96186803294
Federico Alie <79532224+federicoalie@users.noreply.github.com>
2024-12-07 08:47:27 +0700

    update clearer logging time

--- utils/logger.js
+++ utils/logger.js
@@ -1,7 +1,8 @@
 import chalk from 'chalk';
 
 export function logger(message, value = '', level = 'info') {
-    const now = new Date().toISOString();
+    const now = new Date();
+    const formattedDate = `${String(now.getDate()).padStart(2, '0')}/${String(now.getMonth() + 1).padStart(2, '0')}/${now.getFullYear()} ${String(now.getHours()).padStart(2, '0')}:${String(now.getMinutes()).padStart(2, '0')}:${String(now.getSeconds()).padStart(2, '0')}`;
     const levels = {
         info: chalk.greenBright,
         warn: chalk.yellowBright,
@@ -11,5 +12,5 @@ export function logger(message, value = '', level = 'info') {
     };
     
     const log = levels[level] || chalk.whiteBright;
-    console.log(log(`[${now}] [${level.toUpperCase()}]: ${message}`, chalk.yellowBright(value)));
+    console.log(log(`[${formattedDate}] [${level.toUpperCase()}]: ${message}`, chalk.yellowBright(value)));
 }
